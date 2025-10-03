0e67fc8581cf1ce87e6c2bf002937f084fde3bc2
James <123058@combertonvc.org>
2025-10-03 12:30:45 +0100

    Create example.js

---
+++ example.js
@@ -0,0 +1,27 @@
+script.js
+
+
+var num = 5;
+var not_num = "hello";
+
+function isNum(value){if(value-value==0){return true;}else{return false;};};
+
+// num
+if (isNum(num)) {
+    // is a number
+    console.log(num +" is a number");
+} else {
+    // not a number
+    console.log(num +" is not a number");
+};
+
+// not_num
+if (isNum(not_num)) {
+    // is a number
+    console.log(not_num +" is a number");
+} else {
+    // not a number
+    console.log(not_num +" is not a number");
+};
+
+
